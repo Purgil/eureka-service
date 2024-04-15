855307904ce98cd1e6f17e82234cf28506622a5b
ish <tmdghks0615@gmail.com>
2024-04-15 22:14:50 +0900

    first commit

---
+++ src/test/kotlin/io/purgil/eureka/EurekaServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.purgil.eureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
